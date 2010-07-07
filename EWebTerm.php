fe50acaf2e8dc938b5e9ef6db72c0838751c8657
Dimitrios Meggidis <tydeas.dr@gmail.com>
2010-07-08 02:48:06 +0300

    initialize project

---
+++ EWebTerm.php
@@ -0,0 +1,69 @@
+<?php
+class EWebTerm extends CWidget 
+{
+    public $htmlOptions;
+    public $help;
+    public $cssFile;
+    public $welcome;
+    public $width;
+    public $height;
+    public $commands;
+    protected $baseUrl;
+    protected $id;
+    public function init() 
+    {   
+        $this->id = (isset($this->htmlOptions['id'])) ?
+            $this->htmlOptions['id'] : 'wterm';
+
+        $dir=dirname(__FILE__).DIRECTORY_SEPARATOR.'wterm';
+        $this->baseUrl = Yii::app()->getAssetManager()->publish($dir);
+        $cs = Yii::app()->clientScript;
+        $cs->registerScriptFile($this->baseUrl.'/wterm.jquery.js');
+        if ( $this->cssFile !== null && $this->cssFile !== false ) {
+            $cs->registerCssFile($this->cssFile);
+        } else {
+            $cs->registerCssFile($this->baseUrl.'/wterm.css');
+        }
+        if ( !isset($this->welcome)) {
+            $this->welcome = 'Welcome to WTerm. To Begin Using type <strong>help</strong>';
+        }
+        if ( !isset($this->width)) {
+            $this->width = '100%';
+        }
+        if ( !isset($this->height)) {
+            $this->height = '100%';
+        }
+        $cs->registerScript(
+            'Yii.EWebTerm#'.$this->id,
+            "$('#$this->id').wterm({".
+            " WIDTH: '$this->width' , HEIGHT: '$this->height' ,".
+            " WELCOME_MESSAGE: '$this->welcome' ".
+            "});"
+        );
+        $this->registerCommands($cs);
+    }
+    public function run()
+    {
+        echo CHtml::tag('div', array('id'=>$this->id));
+    }
+    protected function registerCommands( $cs )
+    {
+        foreach ( $this->commands as $name => $attr ) {
+            $cs->registerScript(
+                $this->id."_".$name,
+                "$.register_command( '$name' , ".$attr['return'].");",
+                CClientScript::POS_END
+            );
+        }
+    }
+
+/*      $options=$this->getClientOptions();
+        $options=$options===array()?'{}' : CJavaScript::encode($options);
+        
+        //$cs->registerScript('Yii.CJsTree#'.$id,"$(function () { $(\"#{$id}\").tree($options); });");
+        $cs->registerScript('Yii.CJsTree#'.$id,"$(\"#{$id}\").tree($options);");
+
+        if($this->cssFile !== null && $this->cssFile !== false)
+            $cs->registerCssFile($this->cssFile); */
+
+}
