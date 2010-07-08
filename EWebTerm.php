6f37e1da868c9b349406704199a8c6f3f11f088b
Dimitrios Meggidis <tydeas.dr@gmail.com>
2010-07-08 23:11:08 +0300

    Code clean up

--- EWebTerm.php
+++ EWebTerm.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * EWebTerm
  *
  * DESCRIPTION
@@ -7,25 +7,47 @@
  *  This is a Yii framework extension that implements
  * the jQuery Wterm plugin ( http://plugins.jquery.com/project/wterm )
  *
- * @version 0.1
- * @author Dimitrios Meggidis: tydeas.dr[at]gmail[dot]com
- * @link http://github.com/dmtrs/EWebTerm
+ *
+ * @version 0.15
+ * @author  Dimitrios Meggidis <tydeas.dr@gmail.com>
+ * @link    http://github.com/dmtrs/EWebTerm
  **/
-class EWebTerm extends CWidget 
+
+class EWebTerm extends CWidget
 {
     public $htmlOptions;
-    public $help;
     public $cssFile;
-    public $welcome;
-    public $width;
-    public $height;
     public $commands;
+    public $properties;
     protected $baseUrl;
     protected $id;
+    protected $avprop = array(
+        'PS1',                //Primary Prompt ( Defaults to 'wterm $' )
+        'TERMINAL_CLASS',     //Class name that apply to terminal Container ( Defaults to 'wterm_terminal' )
+        'PROMPT_CLASS',       //Class name that apply to prompt ( Defaults to 'wterm_prompt' )
+        'THEME_CLASS_PREFIX', //Theme Class Name that will be prefixed to themes for example theme 'subbeam' will
+                      //automatically become 'prefix_subbeam'
+        'DEFAULT_THEME',   //Default Theme Class Name defaults to 'green_on_black'
+        'HIGHLIGHT_CLASS', //Class name to apply to highlighted text
+        'KEYWORD_CLASS',   //Class name to apply to keywords
+        'WIDTH',           //Width for container
+        'HEIGHT',          //Height for container
+        'WELCOME_MESSAGE', //Default Welcome Message to show when the terminal is first initialized
+        'NOT_FOUND',       //Message to show when a command is not found
+                   //'CMD' in this string will automatically be
+                   //translated to command user typed in
+        'HELP',            //Help not finalized
+        'AUTOCOMPLETE',    //Boolean, Autocomplete enabled/disabled ( By Default Enabled )
+        'HISTORY',         //Boolean, History enabled/disabled ( By Default Enabled )
+        'AJAX_METHOD',     // HTTP Method to call must be GET/POST ( Defaults to GET )
+        'AJAX_PARAM'       //Parameter to send the command in for HTTP request ( Default to tokens )
+    );
+
     public function init() 
-    {   
+    { 
         $this->id = (isset($this->htmlOptions['id'])) ?
             $this->htmlOptions['id'] : 'wterm';
+        $jsen = array();
 
         $dir=dirname(__FILE__).DIRECTORY_SEPARATOR.'wterm';
         $this->baseUrl = Yii::app()->getAssetManager()->publish($dir);
@@ -36,28 +58,29 @@ class EWebTerm extends CWidget
         } else {
             $cs->registerCssFile($this->baseUrl.'/wterm.css');
         }
-        if ( !isset($this->welcome)) {
-            $this->welcome = 'Welcome to WTerm. To Begin Using type <strong>help</strong>';
+
+        if ( isset($this->properties) ) {
+            $prs = array_change_key_case($this->properties, CASE_UPPER);
+            $this->avprop = array_flip($this->avprop);
+            $prs = array_intersect_key($prs, $this->avprop);
+    
+            $jsen = CJavaScript::encode($prs);
+
         }
-        if ( !isset($this->width)) {
-            $this->width = '100%';
-        }
-        if ( !isset($this->height)) {
-            $this->height = '100%';
-        }
+        $ops = (!empty($jsen)) ? $jsen : '{}' ;
+
         $cs->registerScript(
             'Yii.EWebTerm#'.$this->id,
-            "$('#$this->id').wterm({".
-            " WIDTH: '$this->width' , HEIGHT: '$this->height' ,".
-            " WELCOME_MESSAGE: '$this->welcome' ".
-            "});"
+            "$('#$this->id').wterm($ops);"
         );
         $this->registerCommands($cs);
     }
+
     public function run()
     {
         echo CHtml::tag('div', array('id'=>$this->id));
     }
+
     protected function registerCommands( $cs )
     {
         foreach ( $this->commands as $name => $attr ) {
@@ -68,14 +91,4 @@ class EWebTerm extends CWidget
             );
         }
     }
-
-/*      $options=$this->getClientOptions();
-        $options=$options===array()?'{}' : CJavaScript::encode($options);
-        
-        //$cs->registerScript('Yii.CJsTree#'.$id,"$(function () { $(\"#{$id}\").tree($options); });");
-        $cs->registerScript('Yii.CJsTree#'.$id,"$(\"#{$id}\").tree($options);");
-
-        if($this->cssFile !== null && $this->cssFile !== false)
-            $cs->registerCssFile($this->cssFile); */
-
 }
